edcdd2e8eac370c39d254a852271562b8acd37f0
FinalLC <djh_lc@qq.com>
2017-11-13 20:15:41 +0800

    添加了文件读取的功能，又是里程碑的一步！

--- front/static/lib/main.js
+++ front/static/lib/main.js
@@ -1,13 +1,14 @@
 window.onload = function(){
-	
+	//全屏功能的实现
 	var fullscr = document.getElementById('fullscr');
 	fullscr.onclick = function(){
 		console.log("fullScren~~~");
 		editor.setOption("fullScreen",true);
-	    alert("~退出全屏请按F11~");
+		alert("~退出全屏请按F11~");
 	}
 
-	var theme1 = document.getElementById('blackboard');
+	//5种主题的设置
+	var theme1 = document.getElementById('blackboard');//theme----blackboard
 	theme1.onclick = function(){
 		console.log("blackboard~~~");
 		editor.setOption("theme","blackboard");
@@ -15,7 +16,7 @@ window.onload = function(){
 	    //alert("~成功更换主题1~");
 	}
 
-	var theme2 = document.getElementById('eclipse');
+	var theme2 = document.getElementById('eclipse');//theme----eclipse
 	theme2.onclick = function(){
 		console.log("eclipse~~~");
 		editor.setOption("theme",'eclipse');
@@ -23,7 +24,7 @@ window.onload = function(){
 	    //alert("~成功更换主题2~");
 	}
 
-	var theme3 = document.getElementById('monokai');
+	var theme3 = document.getElementById('monokai');//theme----monokai
 	theme3.onclick = function(){
 		console.log("monokai~~~");
 		editor.setOption("theme",'monokai');
@@ -31,7 +32,7 @@ window.onload = function(){
 	    //alert("~成功更换主题3~");
 	}
 
-	var theme4 = document.getElementById('night');
+	var theme4 = document.getElementById('night');//theme----night
 	theme4.onclick = function(){
 		console.log("night~~~");
 		editor.setOption("theme",'night');
@@ -39,7 +40,7 @@ window.onload = function(){
 	    //alert("~成功更换主题4~");
 	}
 
-	var theme5 = document.getElementById('seti');
+	var theme5 = document.getElementById('seti');//theme----seti
 	theme5.onclick = function(){
 		console.log("seti~~~");
 		editor.setOption("theme",'seti');
@@ -47,8 +48,32 @@ window.onload = function(){
 	    //alert("~成功更换主题5~");
 	}
 
+	//本地文件的读取
+	var lopenf = document.getElementById('import');
+	lopenf.onclick = function(){
+		document.getElementById('files').click();
+	}
+	var file = document.getElementById('files');
+	file.onchange = function(){
+		console.log('reading file begins!!!')
+		var selectedFile = document.getElementById("files").files[0];
+		//获取读取的File对象
+		console.log(selectedFile);
+    	var name = selectedFile.name;//读取选中文件的文件名
+    	var size = selectedFile.size;//读取选中文件的大小
+    	console.log("文件名:"+name+"大小："+size);
 
-};
+    	var reader = new FileReader();//这里是核心！！！读取操作就是由它完成的。
+    	reader.readAsText(selectedFile);//读取文件的内容
+
+    	reader.onload = function(){
+        	//console.log(this.result);//当读取完成之后会回调这个函数，
+        	//然后此时文件的内容存储到了result中。直接操作即可。
+        	editor.setValue(this.result);
+    	};
+    }
+}
+
 // function fullScr()
 // {
 // 	console.log("fullScren~~~");
